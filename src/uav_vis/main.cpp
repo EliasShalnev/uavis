9511748019f7ec48f6f067d6ee746f53b4e897a7
Elias Shalnev <shalnev.i@iias.spb.su>
2021-09-07 17:16:22 +0300

    Namespace check.

--- src/uav_vis/main.cpp
+++ src/uav_vis/main.cpp
@@ -1,4 +1,5 @@
 #include <ros/init.h>
+#include <ros/io.h>
 
 #include "uav_vis/Parameters.h"
 #include "uav_vis/UavVis.h"
@@ -10,6 +11,14 @@ const std::string nodeName = "uav_vis";
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, nodeName);
+
+    std::string boardName = ros::this_node::getNamespace();
+    if( boardName.find("bomber") != std::string::npos ) 
+    {
+        ROS_ERROR_STREAM("Unknown board name: " << boardName 
+                         << ". It should be \"/bomber\".");
+        return -1;
+    }
 
     auto parameters = Parameters::getInstance();
     parameters->parseArgs(argc, argv);
